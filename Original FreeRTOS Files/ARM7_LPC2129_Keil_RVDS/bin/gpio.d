054970bfa1fbb27bed0533b9708dfb616a85dc08
Mohammed Abdelalim <MohamedLemo14@gmail.com>
2021-04-14 17:19:04 +0200

    Adding the original FreeRTOS source files

---
+++ Original FreeRTOS Files/ARM7_LPC2129_Keil_RVDS/bin/gpio.d
@@ -0,0 +1,6 @@
+.\bin\gpio.o: Starter_Files_V0\source\GPIO.c
+.\bin\gpio.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\bin\gpio.o: .\Starter_Files_V0\lib\bit_math.h
+.\bin\gpio.o: .\Starter_Files_V0\header\GPIO.h
+.\bin\gpio.o: .\Starter_Files_V0\header\GPIO_cfg.h
+.\bin\gpio.o: C:\Keil_v5\ARM\INC\Philips\lpc21xx.h
---
+++ Original FreeRTOS Files/ARM7_LPC2129_Keil_RVDS/bin/gpio_cfg.d
@@ -0,0 +1,4 @@
+.\bin\gpio_cfg.o: Starter_Files_V0\source\GPIO_cfg.c
+.\bin\gpio_cfg.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\bin\gpio_cfg.o: .\Starter_Files_V0\header\GPIO.h
+.\bin\gpio_cfg.o: .\Starter_Files_V0\header\GPIO_cfg.h
